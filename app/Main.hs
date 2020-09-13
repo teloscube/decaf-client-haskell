dc91518eecee06edf55b946fc0bbd02c70a49b09
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2020-09-13 20:06:04 +0800

    chore: change the dummy Main implementation to a dummier one

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
-import Lib ( someFunc )
+import qualified Data.ByteString.Char8         as BC
+import           Decaf.Client.Internal.Barista (get, mkBaristaClient, mkPath)
+import           Decaf.Client.Internal.Http    (Credentials(BasicCredentials), mkBaseUrl)
+import           System.Environment            (getArgs)
+
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  let client = mkBaristaClient (mkBaseUrl $ args !! 0) (BasicCredentials (BC.pack $ args !! 1) (BC.pack $ args !! 2))
+  BC.putStrLn =<< get [] (mkPath ["version"]) [] client
