0e5e7e2a619820eead97691e1fb85deda7543b77
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2020-09-25 11:13:35 +0800

    chore: add DecafClient convenience type and smart constructor

--- src/Decaf/Client.hs
+++ src/Decaf/Client.hs
@@ -1,13 +1,19 @@
 -- | @TODO: Provide documentation.@
 --
 module Decaf.Client
-  ( -- * Barista
-    BaristaClient
+  ( -- * Client
+
+    DecafClient(..)
+  , mkDecafClient
+
+    -- * Barista Client
+
+  , BaristaClient
   , mkBaristaClient
   , runBarista
   , runBaristaBS
 
-    -- * Microlot
+    -- * Microlot Client
 
   , MicrolotClient
   , mkMicrolotClient
@@ -17,18 +23,35 @@ module Decaf.Client
   , MicrolotQuery(..)
   , MicrolotResponse(..)
 
+    -- * Request Combinators
+
+  , module Decaf.Client.Internal.Combinators
+
     -- * Common Types
 
   , module Decaf.Client.Internal.Types
 
-    -- * Request Combinators
-
-  , module Decaf.Client.Internal.Combinators
   ) where
 
--- TODO: Re-export top-level definitions.
 
+import Data.Text                         (Text)
 import Decaf.Client.Internal.Barista
 import Decaf.Client.Internal.Combinators hiding (dummyDef)
 import Decaf.Client.Internal.Microlot
-import Decaf.Client.Internal.Types hiding (mkPath)
+import Decaf.Client.Internal.Types       hiding (mkPath)
+
+
+-- | Data definition for a collection of various DECAF API clients.
+data DecafClient = DecafClient
+  { decafClientBarista  :: !BaristaClient   -- ^ DECAF Barista API client.
+  , decafClientMicrolot :: !MicrolotClient  -- ^ DECAF Microlot API client.
+  }
+
+
+-- | Attempts to build a 'DecafClient' with given remote DECAF deployment URL
+-- and authentication credentials.
+mkDecafClient :: Text -> Credentials -> Either String DecafClient
+mkDecafClient d c = do
+  baristaClient <- mkBaristaClient d c
+  microlotClient <- mkMicrolotClient d c
+  Right $ DecafClient baristaClient microlotClient
