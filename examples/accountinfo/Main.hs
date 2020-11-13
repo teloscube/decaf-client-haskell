4990d364ca9bc46c21bf7061c8e752c677a155ba
Mehmet Fatih Köksal <fatihkoksal@gmail.com>
2020-11-13 18:52:40 +0100

    chore: implement pdms client and add example usage

--- examples/accountinfo/Main.hs
+++ examples/accountinfo/Main.hs
@@ -3,21 +3,27 @@
 
 module Main where
 
-import           Data.Aeson                        (FromJSON)
+import           Data.Aeson                        (defaultOptions, FromJSON(..), genericParseJSON, Options(fieldLabelModifier))
+import           Data.Char                         (toLower)
 import qualified Data.Text                         as T
 import           Decaf.Client
                  ( Credentials(KeyCredentials)
                  , DecafClient
                  , decafClientBarista
                  , decafClientMicrolot
+                 , decafClientPdms
                  , microlotResponseData
+                 , pdmsResponseData
                  , mkDecafClient
                  , mkMicrolotQuery'
+                 , mkPdmsQuery'
                  , responseValue
                  , runBarista
                  , runMicrolot
+                 , runPdms
                  )
 import           Decaf.Client.Internal.Combinators (path)
+import           Decaf.Client.Internal.Utils       (applyFirst)
 import           GHC.Generics                      (Generic)
 import           System.Environment                (getEnv)
 import           System.IO                         (hPutStrLn, stderr)
@@ -33,8 +39,9 @@ main = do
   case mkDecafClient apiurl credentials of
     Left err -> hPutStrLn stderr $ "Can not create DECAF client" <> show err
     Right dc -> do
-      printRemoteVersion dc
-      printAccounts dc
+      -- printRemoteVersion dc
+      -- printAccounts dc
+      printPolicies dc
 
 
 data VersionResponse = VersionResponse
@@ -80,3 +87,29 @@ printAccounts client = do
 
 enumerate :: [a] -> [(Int, a)]
 enumerate = zip [1..]
+
+
+data PolicyResponse = PolicyResponse
+  { policies :: ![PolicyResponseItem]
+  } deriving (Generic, Show)
+
+instance FromJSON PolicyResponse
+
+
+data PolicyResponseItem = PolicyResponseItem
+  { policyId   :: !T.Text
+  , policyCode :: !T.Text
+  } deriving (Generic, Show)
+
+instance FromJSON PolicyResponseItem where
+  parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = applyFirst toLower . drop 6 }
+
+printPolicies :: DecafClient -> IO ()
+printPolicies client = do
+  let pdms = decafClientPdms client
+  response <- runPdms (mkPdmsQuery' "query {\n policies: policy {\nid\ncode\n} }") pdms
+  let pols = policies . pdmsResponseData . responseValue $ response
+  putStrLn "Policies:"
+  mapM_ printPolicy (enumerate pols)
+  where
+    printPolicy (count, PolicyResponseItem policyid code) = putStrLn $ printf "%d. [%s] %s" count policyid (T.unpack code) 
--- src/Decaf/Client.hs
+++ src/Decaf/Client.hs
@@ -25,6 +25,17 @@ module Decaf.Client
   , MicrolotQuery(..)
   , MicrolotResponse(..)
 
+
+    -- * Pdms Client
+
+  , PdmsClient
+  , mkPdmsClient
+  , runPdms
+  , mkPdmsQuery
+  , mkPdmsQuery'
+  , PdmsQuery(..)
+  , PdmsResponse(..) 
+
     -- * Request Combinators
 
   , module Decaf.Client.Internal.Combinators
@@ -40,6 +51,7 @@ import Data.Text                         (Text)
 import Decaf.Client.Internal.Barista
 import Decaf.Client.Internal.Combinators hiding (_dummyDef)
 import Decaf.Client.Internal.Microlot
+import Decaf.Client.Internal.Pdms
 import Decaf.Client.Internal.Remote      (parseRemote)
 import Decaf.Client.Internal.Types       hiding (mkPath)
 
@@ -48,10 +60,11 @@ import Decaf.Client.Internal.Types       hiding (mkPath)
 data DecafClient = DecafClient
   { decafClientBarista  :: !BaristaClient   -- ^ DECAF Barista API client.
   , decafClientMicrolot :: !MicrolotClient  -- ^ DECAF Microlot API client.
+  , decafClientPdms     :: !PdmsClient      -- ^ DECAF Pdms API client.
   }
 
 
 -- | Attempts to build a 'DecafClient' with given remote DECAF deployment URL
 -- and authentication credentials.
 mkDecafClient :: DecafClientM m => Text -> Credentials -> m DecafClient
-mkDecafClient d c = (DecafClient <$> (`mkBaristaClient` c) <*> (`mkMicrolotClient` c)) <$> parseRemote d
+mkDecafClient d c = (DecafClient <$> (`mkBaristaClient` c) <*> (`mkMicrolotClient` c) <*> (`mkPdmsClient` c)) <$> parseRemote d
