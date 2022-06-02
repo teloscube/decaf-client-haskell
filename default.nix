0793fcf12818dd40e4bec4f504a6c8e1723415a1
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2022-06-02 10:10:04 +0800

    chore(dev): adopt haskell.nix

--- default.nix
+++ default.nix
@@ -1,2 +1,17 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8107" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./decafcli.nix { }
+let
+  ## Read in the Niv sources:
+  sources = import ./nix/sources.nix { };
+
+  ## Fetch the haskell.nix commit we have pinned with Niv:
+  haskellNix = import sources.haskellNix { };
+
+  ## Import nixpkgs and pass the haskell.nix provided nixpkgsArgs
+  pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
+in
+pkgs.haskell-nix.project {
+  ## Note that 'cleanGit' cleans a source directory based on the files known by git.
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "haskell-nix-project";
+    src = ./.;
+  };
+}
