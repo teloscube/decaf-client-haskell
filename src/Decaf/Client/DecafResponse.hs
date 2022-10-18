1b57595958e1abe6c1f80853b4bc5d6adc5715ca
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2022-10-18 11:46:38 +0800

    chore(deps): drop Aeson <2 support

--- src/Decaf/Client/DecafResponse.hs
+++ src/Decaf/Client/DecafResponse.hs
@@ -1,18 +1,12 @@
 -- | This module provides definitions to work with DECAF client responses.
-{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Decaf.Client.DecafResponse where
 
 import           Data.Aeson          ((.:))
 import qualified Data.Aeson          as Aeson
-import           Network.HTTP.Types  (ResponseHeaders, Status)
-
-#if MIN_VERSION_aeson(2,0,0)
 import qualified Data.Aeson.KeyMap
-#else
-import qualified Data.HashMap.Strict as HM
-#endif
+import           Network.HTTP.Types  (ResponseHeaders, Status)
 
 
 -- | Data definition for DECAF client response values.
@@ -33,11 +27,6 @@ data DecafGraphqlQueryResult a =
 
 instance Aeson.FromJSON a => Aeson.FromJSON (DecafGraphqlQueryResult a) where
   parseJSON = Aeson.withObject "DecafGraphqlQueryResult" $ \o -> do
-#if MIN_VERSION_aeson(2,0,0)
-    let merrs = Data.Aeson.KeyMap.lookup "errors" o
-#else
-    let merrs = HM.lookup "errors" o
-#endif
-    case merrs of
+    case Data.Aeson.KeyMap.lookup "errors" o of
       Nothing -> DecafGraphqlQueryResultSuccess <$> o .: "data"
       Just va -> pure (DecafGraphqlQueryResultFailure va)
